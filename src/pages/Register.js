c235a5cce489a23d2cb36f0ceb86c5da6da0c237
dennis <wakahiad@gmail.com>
2022-06-21 15:48:55 +0300

    toggle member

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,50 @@
+import {FormRow, Logo} from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+import {useState} from "react";
+
+
+const initialState = {
+    name: '', email: '', password: '', isMember: true
+}
+
 const Register = () => {
-    return <h4>register</h4>
+    const [values, setValues] = useState(initialState)
+
+    const handleChange = (e) => {
+
+    }
+
+    const toggleMember = () => {
+        setValues({...values, isMember: !values.isMember})
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <Wrapper className='full-page'>
+            <form className='form' onSubmit={onSubmit}>
+                <Logo/>
+                <h3>{values.isMember ? 'Login' : 'Register'}</h3>
+                {!values.isMember && <FormRow type='text' name='name' value={values.name} handleChange={handleChange}/>}
+                <FormRow type='email' name='email' value={values.email} handleChange={handleChange}/>
+                <FormRow type='password' name='password' value={values.password} handleChange={handleChange}/>
+                <button type='submit' className='btn btn-block'>
+                    submit
+                </button>
+                <p>
+                    {values.isMember ? 'Not a member yet?' : 'Already a member?'}
+                    <button
+                        type='button'
+                        onClick={toggleMember}
+                        className='member-btn'>
+                        {values.isMember ? 'Register' : 'Login'}
+                    </button>
+                </p>
+            </form>
+        </Wrapper>
+    )
 }
 
 export default Register;
