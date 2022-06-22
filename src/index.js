48e5a63069add42a0582e75432f468292fcdff86
dennis <wakahiad@gmail.com>
2022-06-22 19:50:14 +0300

    react version switch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {createRoot} from "react-dom/client";
+import ReactDOM from 'react-dom';
 import 'normalize.css'
 import './index.css';
 import App from './App';
@@ -7,15 +7,13 @@ import {Provider} from "react-redux";
 import {store} from "./store";
 
 
-
 const container = document.getElementById('root');
 
-const root = createRoot(container)
-
-root.render(
+ReactDOM.render(
     <Provider store={store}>
         <App/>
-    </Provider>
+    </Provider>,
+    container
 )
 
 
